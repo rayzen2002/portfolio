b4658c7c45f9af84703efdd2dbbd85e292a421c5
rayzen2002 <emanuelzsmj@gmail.com>
2024-03-12 20:05:59 -0300

    Adding Theme Toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 import './globals.css'
+import { ThemeProvider } from '@/components/ThemeProvider'
+import { ModeToggle } from '@/components/ModeToggle'
 
 export const metadata = {
   title: 'Emanuel',
@@ -10,15 +12,24 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
+
       <body className=" bg-gray-800">
         <main className=" flex min-h-screen flex-col bg-gray-800">
           <div className="flex h-36 max-w-full items-center justify-center gap-8 border-b-4 border-black bg-gray-800 text-3xl md:text-2xl xl:text-xl ">
             <a href="/">Home</a>
             <a href="/projetos">Projetos</a>
             <a href="/about">Sobre</a>
+            <ModeToggle />
           </div>
           <div className="flex min-h-0 max-w-full flex-1 items-start justify-center rounded-xl pt-6">
-            {children}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
           </div>
         </main>
       </body>
